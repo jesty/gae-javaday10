f41e5779112b9250b40a20e581b0b8077a55bcfa
davidecerbo <davidecerbo@b0325e3e-0c2a-11df-acff-d76401ec9c37>
2010-01-28 17:27:15 +0000

    version 2 of project with cache and mail example

---
+++ trunk/gaestbook-gae-plus/src/main/java/it/jesty/gaestbook/service/EmailServiceImpl.java
@@ -0,0 +1,39 @@
+package it.jesty.gaestbook.service;
+
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailServiceImpl implements EmailService {
+	
+	private static final Logger log = Logger.getLogger(EmailServiceImpl.class.getName());
+
+	@Override
+	public void sendMail(String from, String to, String subject, String text) {
+		Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        try {
+        	
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress(from));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            msg.setSubject(subject);
+            msg.setText(text);
+            Transport.send(msg);
+
+        } catch (Exception e) {
+            log.severe("Error sending mail to " + to + ". Error: " + e.getMessage());
+        }
+		
+	}
+
+}
